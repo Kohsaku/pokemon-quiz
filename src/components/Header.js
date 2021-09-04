5daed75acffc1be254b06dc816ca082d689fd2cf
Kohsaku <kosakuhayashi@gmail.com>
2021-09-05 01:46:42 +0900

    making sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { auth } from '../api/firebase';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
+
+import './Header.css';
 
 const Header = props => {
   const user = auth.currentUser;
   return(
     <div className="App-header">
+      <IconButton onClick={props.handleSidebar}>
+        <MenuIcon color="primary" />
+      </IconButton>
       <h2>Pokemon Quiz</h2>
       {user ?  (
         <Link to="/" onClick={props.handleSignOut}>
