bdaf962518a3ec5cdcae3eecf8b6d97da0cabc17
Kohsaku <kosakuhayashi@gmail.com>
2021-11-17 00:38:16 +0900

    redesigned loginPage and signinPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,68 +1,160 @@
-import React, { useState } from 'react';
-import CustomButton from './customButton';
-import { auth, signInWithGoogle } from '../api/firebase';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { auth, signInWithGoogle } from "../api/firebase";
+import { useHistory } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Modal from "@material-ui/core/Modal";
+import IconButton from "@material-ui/core/IconButton";
+import SendIcon from "@material-ui/icons/Send";
+import { makeStyles } from "@material-ui/core";
+
+import { Link } from "react-router-dom";
+
+import "./LoginPage.css";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    outline: "none",
+    position: "absolute",
+    width: 400,
+    borderRadius: 10,
+    backgroundColor: "white",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(10),
+  },
+}));
 
 const LoginPage = () => {
+  const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [resetEmail, setResetEmail] = useState("");
   const history = useHistory();
 
-  const handleSubmit = async event => {
+  const sendResetEmail = async (event) => {
+    await auth
+      .sendPasswordResetEmail(resetEmail)
+      .then(() => {
+        setOpenModal(false);
+        setResetEmail("");
+      })
+      .catch((err) => {
+        alert(err.message);
+        setResetEmail("");
+      });
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       await auth.signInWithEmailAndPassword(email, password);
-      setEmail('');
-      setPassword('');
+      setEmail("");
+      setPassword("");
 
-      history.push('/');
+      history.push("/");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  const emailHandleChange = event => {
+  const emailHandleChange = (event) => {
     const name = event.target.value;
     setEmail(name);
-  }
+  };
 
-  const passwordHandleChange = event => {
+  const passwordHandleChange = (event) => {
     const name = event.target.value;
     setPassword(name);
-  }
+  };
 
   return (
     <div className="LoginPage">
-      Login Page
-      <form onSubmit={handleSubmit}>
-        <label>
-          email
-          <input 
-            name="email" 
-            type="email" 
+      <Container className="FormContainer" component="main" maxWidth="sm">
+        <form onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
             onChange={emailHandleChange}
-            value={email} 
-            required 
+            value={email}
+            required
           />
-        </label>
-        <label>
-          password
-          <input 
-            name="password" 
-            type="password" 
+          <TextField
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
             onChange={passwordHandleChange}
-            value={password} 
-            required 
+            value={password}
+            required
           />
-        </label>
-        <div>
-          <CustomButton label="ログイン" />
-          <button onClick={signInWithGoogle}>Google SignIn</button>
-        </div>
-      </form>
+          <span className="Login_FormButton">
+            <Button type="submit" fullWidth variant="contained">
+              Sign In
+            </Button>
+          </span>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={signInWithGoogle}
+          >
+            Google Sign In
+          </Button>
+          <Grid container className="FormGrid">
+            <Grid item xs>
+              <span onClick={() => setOpenModal(true)}>Forgot password?</span>
+            </Grid>
+            <Grid item>
+              <Link to="/signup">Don't have an account? Sign Up</Link>
+            </Grid>
+          </Grid>
+        </form>
+        <Modal open={openModal} onClose={() => setOpenModal(false)}>
+          <div style={getModalStyle()} className={classes.modal}>
+            <div>
+              <TextField
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                type="email"
+                name="email"
+                label="Reset E-mail"
+                value={resetEmail}
+                onChange={(event) => {
+                  setResetEmail(event.target.value);
+                }}
+              />
+              <IconButton onClick={sendResetEmail}>
+                <SendIcon />
+              </IconButton>
+            </div>
+          </div>
+        </Modal>
+      </Container>
     </div>
   );
-}
+};
 
 export default LoginPage;
--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
-import { auth, createUserProfileDocument } from '../api/firebase';
-import CustomButton from './customButton';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { auth, createUserProfileDocument } from "../api/firebase";
+import CustomButton from "./customButton";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+
+import "./SignupPage.css";
 
 const SignupPage = () => {
-  const [displayName, setDisplayName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const history = useHistory();
 
-  const handleSubmit = async event => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if(password !== confirmPassword) {
+    if (password !== confirmPassword) {
       alert("passwords don't match");
       return;
-    } 
+    }
 
     try {
       const { user } = await auth.createUserWithEmailAndPassword(
@@ -28,86 +37,112 @@ const SignupPage = () => {
 
       await createUserProfileDocument(user, { displayName });
 
-      setDisplayName('');
-      setEmail('');
-      setPassword('');
-      setConfirmPassword('');
+      setDisplayName("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
 
-      history.push('/');
-
+      history.push("/");
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-  }
+  };
 
-  const displayNameHandleChange = event => {
+  const displayNameHandleChange = (event) => {
     setDisplayName(event.target.value);
-    console.log(displayName);
-  }
+  };
 
-  const emailHandleChange = event => {
+  const emailHandleChange = (event) => {
     setEmail(event.target.value);
-    console.log(email);
-  }
+  };
 
-  const passwordHandleChange = event => {
+  const passwordHandleChange = (event) => {
     setPassword(event.target.value);
-    console.log(password);  
-  }
+  };
 
-  const confirmPasswordHandleChange = event => {
+  const confirmPasswordHandleChange = (event) => {
     setConfirmPassword(event.target.value);
-    console.log(confirmPassword);
-  }
+  };
 
   return (
     <div>
-      Sign Up
       <form onSubmit={handleSubmit}>
-        <label>
-          name
-          <input 
-            name="displayName" 
-            type="text" 
-            onChange={displayNameHandleChange}
-            value={displayName} 
-            required 
-          />
-        </label>
-        <label>
-          email
-          <input 
-            name="email" 
-            type="email" 
-            onChange={emailHandleChange}
-            value={email} 
-            required 
-          />
-        </label>
-        <label>
-          password
-          <input 
-            name="password" 
-            type="password" 
-            onChange={passwordHandleChange}
-            value={password} 
-            required 
-          />
-        </label>
-        <label>
-          confirm password
-          <input 
-            name="confirmPassword" 
-            type="password" 
-            onChange={confirmPasswordHandleChange}
-            value={confirmPassword} 
-            required 
-          />
-        </label>
-        <CustomButton label='submit'/>
+        <Container
+          className="Signup-FormContainer"
+          component="main"
+          maxWidth="xs"
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="Name"
+                name="Name"
+                required
+                fullWidth
+                id="Name"
+                label="Name"
+                autoFocus
+                onChange={displayNameHandleChange}
+                value={displayName}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={emailHandleChange}
+                value={email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                onChange={passwordHandleChange}
+                value={password}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="confirm-password"
+                label="Confirm Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                onChange={confirmPasswordHandleChange}
+                value={confirmPassword}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            className="Signup-FormButton"
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link to="/login">Already have an account? Sign in</Link>
+            </Grid>
+          </Grid>
+        </Container>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SignupPage;
